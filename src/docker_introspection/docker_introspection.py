752d8d43f1b6bd6e93312a80be117667ea36541a
Ilya Sher <ilya.sher@coding-knight.com>
2014-11-20 18:58:33 +0200

    Refactor: get rid of DOCKER_CONFIG_ITEMS, no_auth->do_auth

--- src/docker_introspection/docker_introspection.py
+++ src/docker_introspection/docker_introspection.py
@@ -5,38 +5,12 @@ from flask import Flask, request
 from flask.ext.restful import abort, Resource, Api
 from argparse import ArgumentParser
 
-DOCKER_CONFIG_ITEMS = [u'Env',
-                       u'Hostname',
-                       u'Entrypoint',
-                       u'PortSpecs',
-                       u'Memory',
-                       u'OnBuild',
-                       u'OpenStdin',
-                       u'Cpuset',
-                       u'User',
-                       u'CpuShares',
-                       u'AttachStdout',
-                       u'NetworkDisabled',
-                       u'WorkingDir',
-                       u'Cmd',
-                       u'StdinOnce',
-                       u'AttachStdin',
-                       u'MemorySwap',
-                       u'Volumes',
-                       u'Tty',
-                       u'AttachStderr',
-                       u'Domainname',
-                       u'Image',
-                       u'SecurityOpt',
-                       u'ExposedPorts']
-config_names = { name.lower(): name for name in DOCKER_CONFIG_ITEMS }
-
 app = Flask(__name__)
 api = Api(app)
 
 class DockerContainer(Resource):
     def get(self, container_id, config_item=None):
-        if not app._no_auth:
+        if app._do_auth:
             source_container_id = self._get_source_container_id()
 
             if container_id == '_myself':
@@ -51,12 +25,10 @@ class DockerContainer(Resource):
             abort(404)
 
         if config_item:
-            if not config_item.lower() in config_names: abort(404)
-            config_item = config_names[config_item.lower()]
-            if config_item in container_info['Config']:
-                return container_info['Config'][config_item]
-            else:
-                abort(404)
+            for k in container_info['Config']:
+                if k.lower() == config_item:
+                    return container_info['Config'][k]
+            abort(404)
         else:
             return container_info
 
@@ -72,11 +44,11 @@ def main():
     parser.add_argument("-u", dest='url', default='unix://var/run/docker.sock', help="Docker API url")
     parser.add_argument("-b", dest='bind_address', default='172.17.42.1', help="Address to listen (bind) on")
     parser.add_argument("-d", dest='debug', default=False, help="Debug mode", action='store_true')
-    parser.add_argument("--no-auth", dest='no_auth', default=False, help="Disable source IP checks", action='store_true')
+    parser.add_argument("--no-auth", dest='do_auth', default=True, help="Disable source IP checks", action='store_false')
     opts = parser.parse_args()
 
     app._docker_client = docker.Client(opts.url)
-    app._no_auth = opts.no_auth
+    app._do_auth = opts.do_auth
 
     app.run(debug=opts.debug, port=opts.port, host=opts.bind_address)
 
